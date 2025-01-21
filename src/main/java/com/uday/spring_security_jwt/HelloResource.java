80d8e813e947efa2f71ed8843126459f49c598d7
UDAYBHASKAR\yenne <yenneti.uday@hotmail.com>
2025-01-21 20:38:37 +0530

    implimented Spring security to work with JWT with hard coded user details

--- src/main/java/com/uday/spring_security_jwt/HelloResource.java
+++ src/main/java/com/uday/spring_security_jwt/HelloResource.java
@@ -1,14 +1,50 @@
 package com.uday.spring_security_jwt;
 
+import com.uday.spring_security_jwt.models.AuthenticationRequest;
+import com.uday.spring_security_jwt.models.AuthenticationResponse;
+import com.uday.spring_security_jwt.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloResource {
 
-    @GetMapping("/")
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService myUserDetailsService;
+
+    @Autowired
+    private JwtUtil jwtTokenUtil;
+
+    @GetMapping("/hello")
     public String hello() {
         return "Hello World";
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+        try{
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrect username or password", e);
+        }
+        final UserDetails userDetails = myUserDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+        final String jwt = jwtTokenUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
     }
 
 }
---
+++ src/main/java/com/uday/spring_security_jwt/models/AuthenticationResponse.java
@@ -0,0 +1,13 @@
+package com.uday.spring_security_jwt.models;
+
+public class AuthenticationResponse {
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+}
